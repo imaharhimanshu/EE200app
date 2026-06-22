60d901a96a972a762d9cc3d72afbca8fb4f07c8c
Himanshu Meena <himanshumeenagagwana@gmail.com>
2026-06-22 21:18:10 +0530

    Delete test_match.py

--- test_match.py
+++
@@ -1,5 +0,0 @@
-from fingerprint import identify
-
-best_song, S_db, p_times, p_freqs, all_scores, _ = identify("songs/Bohemian Rhapsody.mp3")
-print("Matched:", best_song)
-print(sorted(all_scores.items(), key=lambda x: -x[1])[:5])
